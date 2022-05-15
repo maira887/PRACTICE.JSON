1b4b221baa4d6e9d7e9e18e7e12bcbdffbfcb53b
maira887 <101581209+maira887@users.noreply.github.com>
2022-05-15 15:11:13 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+ jsonData=
+ [
+ {
+ name: "maira",
+ class: 1,
+ rollno: 3 
+ },
+
+ {
+ name: "hamna",
+ class: 9,
+ rollno: 8 
+ },
+
+ { 
+ name: "laiba",
+ class: 2,
+ rollno: 7 
+ }
+ ]
+ temp=" ";
+ class school{
+//  constructor(name,class,rollno)
+ }
+ {
+ this.name=name,
+ this.class=Class,
+ this.rollno=rollno
+}
