0edb8028a9e21c357374bb1cae2a746d77dc4b70
maira887 <101581209+maira887@users.noreply.github.com>
2022-05-16 20:55:16 +0500

    Add files via upload

---
+++ a.js
@@ -0,0 +1,27 @@
+ = [
+  {
+    name: "maira",
+    class: 1,
+    rollno: 3,
+  },
+
+  {
+    name: "hamna",
+    class: 9,
+    rollno: 8,
+  },
+
+  {
+    name: "laiba",
+    class: 2,
+    rollno: 7,
+  },jsonData
+];
+function adddata(name1, class1, roll1) {
+  data = {
+    name: name1,
+    class: class1,
+    rollno: rollno1,
+  };
+}
+console.log("new user added");
--- index.js
+++ index.js
@@ -1,29 +1,11 @@
- jsonData=
- [
- {
- name: "maira",
- class: 1,
- rollno: 3 
- },
 
- {
- name: "hamna",
- class: 9,
- rollno: 8 
- },
-
- { 
- name: "laiba",
- class: 2,
- rollno: 7 
- }
- ]
- temp=" ";
- class school{
-//  constructor(name,class,rollno)
- }
- {
- this.name=name,
- this.class=Class,
- this.rollno=rollno
-}
+//  temp=" ";
+//  class school{
+//   constructor(name,class,rollno)
+//  }
+//  {
+//  this.name=name,
+//  this.class=Class,
+//  this.rollno=rollno
+//  }
+//  jsondata=jsonData("laiba",2)
